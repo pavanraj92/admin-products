a78dc14652ae252d1a039a6c6b0108e9df9f44a4
Jahnavi <jahnavi.mathur@dotsquares.com>
2025-08-20 14:56:47 +0530

    db changes

--- database/migrations/2025_08_11_000002_create_transactions_table.php
+++ database/migrations/2025_08_11_000002_create_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->foreignId('order_id')->nullable()->constrained('orders')->cascadeOnDelete();
             $table->string('payment_gateway'); // stripe, paypal, etc.
             $table->string('transaction_reference')->nullable();
             $table->decimal('amount', 10, 2);
